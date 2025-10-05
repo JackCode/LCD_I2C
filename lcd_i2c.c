bcfd747709960e1762f4fc19c75c625819ff8efe
jackson <jackson@jackcode.com>
2025-10-04 23:48:30 -0400

    Init Commit

--- lcd_i2c.c
+++ lcd_i2c.c
@@ -1,1 +1,152 @@
+#include "lcd_i2c.h"
+#include <stdbool.h>
+#include <stdint.h>
 
+static struct I2C_Interface i2c;
+static uint8_t device_address = DEFAULT_DEVICE_ADDRESS;
+static uint8_t current_backlight_state = BACKLIGHT_OFF;
+static uint8_t current_display_state = DISPLAYOFF | CURSOROFF | BLINKOFF;
+static uint8_t current_function_state = F_4BITMODE | F_2LINE | F_5x8DOTS;
+static uint8_t current_entry_mode = ENTRYLEFT | ENTRYSHIFTDECREMENT;
+
+static void write_expander(uint8_t data) {
+    i2c.send_start_condition_and_7bit_address(device_address);
+	i2c.send_one_byte_of_data(data | current_backlight_state);
+	i2c.send_stop_condition();
+}
+
+static void pulse_enable_pin(uint8_t data) {
+    write_expander(data | En);
+	i2c.micro_sec_timer(5);
+
+	write_expander(data & ~En);
+	i2c.micro_sec_timer(5);
+}
+
+static void write4bits(uint8_t data) {
+    write_expander(data);
+    pulse_enable_pin(data);
+}
+
+static void send(uint8_t value, uint8_t mode) {
+    uint8_t high_nib = value & 0xF0;
+	uint8_t low_nib = (value << 4) & 0xF0;
+	write4bits(high_nib | mode);
+	write4bits(low_nib |mode);
+}
+
+static void command(uint8_t value) {
+    send(value, 0);
+}
+
+static void clear() {
+    command(CLEAR_DISPLAY);
+    i2c.micro_sec_timer(5000);
+}
+
+static void home() {
+    command(RETURN_HOME);
+    i2c.micro_sec_timer(5000);
+}
+
+static void turn_off_backlight(void) {
+    current_backlight_state = BACKLIGHT_OFF;
+    write_expander(0);
+}
+
+static void turn_on_backlight(void) {
+    current_backlight_state = BACKLIGHT_ON;
+    write_expander(0);
+}
+
+static void turn_on_base_cursor() {
+    current_display_state |= CURSORON;
+    command(DISPLAY_CONTROL | current_display_state);
+}
+
+static void turn_off_base_cursor() {
+    current_display_state &= ~CURSORON;
+    command(DISPLAY_CONTROL | current_display_state);
+}
+
+static void turn_on_blinking_cursor() {
+    current_display_state |= BLINKON;
+	command(DISPLAY_CONTROL | current_display_state);
+}
+
+static void turn_off_blinking_cursor() {
+    current_display_state &= ~BLINKON;
+	command(DISPLAY_CONTROL | current_display_state);
+}
+
+static void scroll_left() {
+    command(CURSOR_DISPLAY_SHIFT | DISPLAYMOVE | MOVELEFT);
+}
+
+static void scroll_right() {
+    command(CURSOR_DISPLAY_SHIFT | DISPLAYMOVE | MOVERIGHT);
+}
+
+static void set_cursor_position(uint8_t row, uint8_t col) {
+    command(SET_DDRAM_ADDRESS | ((col%40) + (row%4 * 0x40)));
+}
+
+static void turn_off_display() {
+    current_display_state &= ~DISPLAYON;
+    command(DISPLAY_CONTROL | current_display_state);
+}
+
+static void turn_on_display() {
+    current_display_state |= DISPLAYON;
+    command(DISPLAY_CONTROL | current_display_state);
+}
+
+static void write(uint8_t value) {
+    send(value, Rs);
+}
+
+static void init(struct I2C_Interface i2c_interface) {
+	// Follow steps in https://cdn.sparkfun.com/assets/9/5/f/7/b/HD44780.pdf
+	// page 46
+	i2c = i2c_interface;
+	i2c.micro_sec_timer(500); // >40ms
+	write4bits(0x03 << 4);
+	i2c.micro_sec_timer(50); // >4.1ms
+	write4bits(0x03 << 4);
+	i2c.micro_sec_timer(1); // >100ns
+	write4bits(0x03 << 4);
+	i2c.micro_sec_timer(1);
+
+	write4bits(0x02 << 4); // Set 4-bit mode
+
+	command(SET_FUNCTION | current_function_state);
+	clear();
+	command(DISPLAY_CONTROL | current_display_state);
+	clear();
+	command(SET_ENTRY_MODE | current_entry_mode);
+	home();
+}
+
+static void init_with_device_address(struct I2C_Interface i2c_interface, uint8_t lcd_address) {
+    device_address = lcd_address;
+    init(i2c_interface);
+}
+
+const struct lcd_i2c LCD_I2C = {
+		.init = init,
+        .init_with_device_address = init_with_device_address,
+		.write = write,
+		.clear = clear,
+		.home = home,
+		.turn_off_backlight = turn_off_backlight,
+		.turn_on_backlight = turn_on_backlight,
+		.turn_on_blinking_cursor = turn_on_blinking_cursor,
+		.turn_off_blinking_cursor = turn_off_blinking_cursor,
+		.scroll_left = scroll_left,
+		.scroll_right = scroll_right,
+		.set_cursor_position = set_cursor_position,
+		.hide_display = turn_off_display,
+		.show_display = turn_on_display,
+		.turn_on_base_cursor = turn_on_base_cursor,
+		.turn_off_base_cursor = turn_off_base_cursor
+};
--- lcd_i2c.h
+++ lcd_i2c.h
@@ -1,1 +1,83 @@
+#ifndef __LCD_I2C__
+#define __LCD_I2C__
 
+#include <stdint.h>
+
+#define DEFAULT_DEVICE_ADDRESS  0x27
+
+/* Commands */
+#define CLEAR_DISPLAY			0x01
+#define RETURN_HOME				0x02
+#define SET_ENTRY_MODE  		0x04
+#define DISPLAY_CONTROL			0x08
+#define CURSOR_DISPLAY_SHIFT	0x10
+#define SET_FUNCTION			0x20
+#define SET_CGRAM_ADDRESS		0x40
+#define SET_DDRAM_ADDRESS		0x80
+
+/* Entry Mode Flags */
+#define ENTRYRIGHT          0x00
+#define ENTRYLEFT           0x02
+#define ENTRYSHIFTINCREMENT 0x01
+#define ENTRYSHIFTDECREMENT 0x00
+
+/* Display Control Flags */
+#define DISPLAYON   0x04
+#define DISPLAYOFF  0x00
+#define CURSORON    0x02
+#define CURSOROFF   0x00
+#define BLINKON     0x01
+#define BLINKOFF    0x00
+
+/* Cursor and Display Shift Flags */
+#define DISPLAYMOVE 0x08
+#define CURSORMOVE  0x00
+#define MOVERIGHT   0x04
+#define MOVELEFT    0x00
+
+/* Set Function Flags */
+#define F_8BITMODE    0x10
+#define F_4BITMODE    0x00
+#define F_2LINE       0x08
+#define F_1LINE       0x00
+#define F_5x10DOTS    0x04
+#define F_5x8DOTS     0x00
+
+/* Backlight Flags */
+#define BACKLIGHT_ON    0X08
+#define BACKLIGHT_OFF	0X00
+
+/* En/R/W Bits */
+#define En 0b00000100  // Enable bit
+#define Rw 0b00000010  // Read/Write bit
+#define Rs 0b00000001  // Register select bit
+
+typedef struct I2C_Interface {
+	void (*send_start_condition_and_7bit_address)(uint8_t address);
+	void (*send_one_byte_of_data)(uint8_t data);
+	void (*send_stop_condition)(void);
+	void (*micro_sec_timer)(uint32_t microseconds);
+} I2C_Interface;
+
+struct lcd_i2c {
+	void (*init)(struct I2C_Interface);
+    void (*init_with_device_address)(struct I2C_Interface, uint8_t lcd_address);
+	void (*write)(uint8_t value);
+	void (*clear)(void);
+	void (*home) (void);
+	void (*turn_off_backlight)(void);
+	void (*turn_on_backlight)(void);
+	void (*turn_on_blinking_cursor)(void);
+	void (*turn_off_blinking_cursor)(void);
+	void (*scroll_right)(void);
+	void (*scroll_left)(void);
+	void (*set_cursor_position)(uint8_t row, uint8_t col);
+	void (*hide_display)(void);
+	void (*show_display)(void);
+	void (*turn_on_base_cursor)(void);
+	void (*turn_off_base_cursor)(void);
+};
+
+extern const struct lcd_i2c LCD_I2C;
+
+#endif
